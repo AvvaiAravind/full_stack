39fa07360cc419392f6d0409d9db7d26981364d9
Avvai Alias Aravind <avvaiaravind@gmail.com>
2025-08-20 09:30:59 +0530

    feat: add create user

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL;
+
+if (!baseURL) {
+  throw new Error("VITE_API_URL is not set");
+}
+
 const api = axios.create({
-  baseURL: "http://localhost:3500",
+  baseURL,
   headers: {
     "Content-Type": "application/json",
   },
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,8 @@
+import UserForm, { UserInput } from "@src/components/custom/UserForm";
 import UserList from "@src/components/custom/UserList";
+import { Button } from "@src/components/ui/button";
 import api from "@src/lib/axios";
+import { Plus } from "lucide-react";
 import { useCallback, useEffect, useState } from "react";
 
 export type User = {
@@ -11,7 +14,7 @@ export type User = {
 const Home = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [isOpen, setIsOpen] = useState(false);
   const fetchUsers = useCallback(async () => {
     setIsLoading(true);
     try {
@@ -28,23 +31,56 @@ const Home = () => {
     fetchUsers();
   }, [fetchUsers]);
 
+  const handleAddUser = useCallback(async (data: UserInput) => {
+    setIsLoading(true);
+    try {
+      const response = await api.post("/api/users", data);
+      setUsers([...users, response.data]);
+      setIsOpen(false);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
   return (
-    <div className="mx-auto h-full w-full max-w-md">
-      <h1 className="text-center text-2xl font-bold">Users</h1>
-      {isLoading && <p className="text-center">Loading...</p>}
-
-      {!isLoading && users.length === 0 && (
-        <p className="text-center">No users found</p>
+    <>
+      {isOpen && (
+        <UserForm
+          onSubmit={handleAddUser}
+          isLoading={isLoading}
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+        />
       )}
 
-      {!isLoading && users.length > 0 && (
-        <ul className="mt-4 flex h-full w-full flex-col items-center gap-2">
-          {users.map((user) => (
-            <UserList key={user._id} {...user} />
-          ))}
-        </ul>
-      )}
-    </div>
+      <div className="mx-auto h-full w-full max-w-md">
+        <div className="flex items-center justify-between">
+          <h1 className="text-center text-2xl font-bold">Users</h1>
+          <Button
+            className="cursor-pointer"
+            type="button"
+            onClick={() => setIsOpen(true)}
+          >
+            <Plus /> Add User
+          </Button>
+        </div>
+        {isLoading && <p className="text-center">Loading...</p>}
+
+        {!isLoading && users.length === 0 && (
+          <p className="text-center">No users found</p>
+        )}
+
+        {!isLoading && users.length > 0 && (
+          <ul className="mt-4 flex h-full w-full flex-col items-center gap-2">
+            {users.map((user) => (
+              <UserList key={user._id} {...user} />
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 };
 
